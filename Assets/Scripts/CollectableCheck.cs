80a4b419b897abc870451002fbc37baf134f1cf1
ahc704 <46978022+ahc704@users.noreply.github.com>
2019-12-05 21:21:37 -0500

    Collectable script added

--- Assets/Scripts/CollectableCheck.cs
+++ Assets/Scripts/CollectableCheck.cs
@@ -1,12 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class CollectableCheck : MonoBehaviour
 {
-    
-    void Update()
+    private int count;
+    public Text countText;
+    public Text interactText;
+    public Text strongholdText;
+
+    void Awake()
     {
-        
+        count = 0;
+        interactText.text = "";
+        countText.text = "";
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            setInteractText("Press [E] to Interact");
+            if (Input.GetKeyDown("e"))
+            {
+                other.gameObject.SetActive(false);
+                setCountText();
+            }
+            setInteractText("Wonder if there's anything else");
+
+        }
+    }
+
+    void setInteractText(string S)
+    {
+        interactText.text = S;
+    }
+
+    void setCountText()
+    {
+        count += 1;
+        countText.text = "Collected:" + count.ToString() + "/3";
+        if(count >= 3)
+        {
+            strongholdText.text = "Stronghold has been revealed";
+        }
     }
 }
